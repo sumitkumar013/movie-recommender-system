f35b50292c1518479bbcdc95bc140f9c1e5d679c
deepranjan71 <deepranjan71@gmail.com>
2026-05-13 12:02:22 +0530

    update

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    recommend_movies =[]
+    recommended_movies = []
     for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
 
-        recommend_movies.append(movies.iloc[i[0]].title)
-
-    return recommend_movies
+    return recommended_movies
 
 movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
